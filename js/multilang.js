eed7dede5b7eb5d152c92687e3db73193ea1f71f
GermainMoreau <germain.moreau@free.fr>
2017-01-29 10:22:24 +0100

    correct indentation

--- js/multilang.js
+++ js/multilang.js
@@ -1,28 +1,27 @@
 function setLanguage(Lang) {
-	changeDisplayOfALanguage('EN','none');
-	changeDisplayOfALanguage('FR','none');
-	changeDisplayOfALanguage('IT','none');
-	changeDisplayOfALanguage(Lang,'block');
+    changeDisplayOfALanguage('EN', 'none');
+    changeDisplayOfALanguage('FR', 'none');
+    changeDisplayOfALanguage('IT', 'none');
+    changeDisplayOfALanguage(Lang, 'block');
 }
 
-function changeDisplayOfALanguage(Lang,kindOfDisplay)
-{
-	var elems = document.getElementsByClassName(Lang);
+function changeDisplayOfALanguage(Lang, kindOfDisplay) {
+    var elems = document.getElementsByClassName(Lang);
 
-	for (var i=0;i<elems.length;i+=1){
-  		elems[i].style.display = kindOfDisplay ;
-	}
-	
-}function defaultLanguage()
-{
-	var userLang = navigator.language || navigator.userLanguage;
+    for (var i = 0; i < elems.length; i += 1) {
+        elems[i].style.display = kindOfDisplay;
+    }
 
-	if (userLang.toUpperCase() != 'FR' && userLang.toUpperCase() != 'IT') {
-			setLanguage('EN');
-	}
-	else{
-		setLanguage(userLang.toUpperCase());
-	}
+}
+function defaultLanguage() {
+    var userLang = navigator.language || navigator.userLanguage;
+
+    if (userLang.toUpperCase() != 'FR' && userLang.toUpperCase() != 'IT') {
+        setLanguage('EN');
+    }
+    else {
+        setLanguage(userLang.toUpperCase());
+    }
 
 
 }
